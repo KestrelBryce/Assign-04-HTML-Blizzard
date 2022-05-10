70d02ef86feda25fd8f20e901d74302bb1b0f77e
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-05-10 15:06:55 +0000

    changed stuff and added picture

--- js/script.js
+++ js/script.js
@@ -14,57 +14,103 @@ function displayGreeting () {
     // Constants
   let TaxRate = 0.13;
 
+  let gAPrice = 1.89;
+
+  let pepsiPrice = 1.79;
+
+  let cocaColaPrice = 1.99;
+
+  let fantaPrice = 1.49;
+
+  let drPepperPrice = 1.59;
+
     // Remember to write || by using shift on \ twice
 
+    // Size calculate process
+  if (BSize == 'small') {
+    BSizeCost = 3.50;
+  }
+
+  else if (BSize == 'medium') {
+    BSizeCost = 4.10;
+  }
+
+  else {
+    BSizeCost = 5.30;
+  }
+
+
+    // Flavour calculate process
+  if () {
+    BFlavCost = ;
+  }
+
+  else if () {
+    BFlavCost = ;
+  }
+
+  else if () {
+    BFlavCost = ;
+
+
+  else if () {
+    BFlavCost = ;
+  }
+
+  else {
+    BFlavCost = ;
+  }
+  
     // Drink calculate Process
-  if (Drink == Ginger Ale) {
-    let DrinkCost = 1.89
+  if (Drink == 'Ginger Ale') {
+    let drinkCost = gAPrice;
   }
 
-  else if (Drink == Pepsi) {
-    let DrinkCost = 1.79
+  else if (Drink == 'Pepsi') {
+    let drinkCost = pepsiPrice;
   }
 
-  else if (Drink == CocaCola) {
-    let DrinkCost = 1.99
+  else if (Drink == 'Coca-Cola') {
+    let drinkCost = cocaColaPrice;
   }
 
-  else if (Drink == Fanta) {
-    let DrinkCost = 1.49
+  else if (Drink == 'Fanta') {
+    let drinkCost = fantaPrice;
   }
 
-  else if (Drink == Dr Pepper) {
-    let DrinkCost = 1.59
+  else if (Drink == 'Dr-Pepper') {
+    let drinkCost = drPepperPrice;
   }
 
-  else if (Drink == No drink) {
-    let DrinkCost = 0
+  else {
+    let drinkCost = 0;
   }
 
     // Variables
-  let YDrinkSub = BSize + BFlav + DrinkCost;
+  //let YDrinkSub = BSize + BFlav + drinkCost;
 
-  let NDrinkSub = BSize + BFlav;
+  let NDrinkSub = BSizeCost + BFlavCost;
 
-
-  if () {
-    document.getElementById("greeting").innerHTML = "You are" + Drink + ", which means you are eligable for a student discount."
+  if (1 > 0) {
+    document.getElementById('greeting').innerHTML = 'hi'
   }
-    
+
+
+
     // Order calculate Process
     // YDrinkSub tax
   //if (DrinkCost > 0) {
     //let YDrinkTax = YDrinkSub * TaxRate
     //let YDrinkTotal = YDrinkSub + YDrinkTax
     //document.getElementById("greeting").innerHTML = "You are " + DrinkCost + ", which means you are eligable for a student discount."
-  }
+  //}
   
     // NDrinkSub tax
   //else if ((userAge >= 12) && (userAge <= 21)) {
   //  document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are eligable for a student discount."
   //}
 
-}
+
 
 
   //else if ((userAge >= 12) && (userAge <= 21)) {
